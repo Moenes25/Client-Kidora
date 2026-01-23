8414f392c36508b7e12d63d849bcdd26bb35b82e
Oumayma Gharbi <gharbioumayma888@gmail.com>
2026-01-23 15:58:07 +0100

    icone enfants pas le meme comme les autres interfaces

--- src/parent/layout/AppHeader.tsx
+++ src/parent/layout/AppHeader.tsx
@@ -79,6 +79,8 @@ const AppHeader: React.FC = () => {
     { 
       name: "Enfants", 
       path: "/parent/enfants",
+
+      
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <circle cx="12" cy="5" r="3"/>
