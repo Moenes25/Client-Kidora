1ac816fb82a9b5f0d8b2b3c77ab2353305e4a136
Oumayma Gharbi <gharbioumayma888@gmail.com>
2026-01-26 11:34:08 +0100

    supprimer icon de sideBar

--- src/educateur/layout/AppHeader.tsx
+++ src/educateur/layout/AppHeader.tsx
@@ -127,7 +127,9 @@ const AppHeader: React.FC = () => {
         <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-indigo-400/20 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4"> */}
        <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
         <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 dark:border-indigo-400/20 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
-          <button
+         
+         
+         {/* <button
             className="items-center justify-center w-10 h-10 border-white/20 text-white rounded-lg z-99999 lg:flex lg:h-11 lg:w-11 lg:border hover:bg-white/10"
             onClick={handleToggle}
             aria-label="Toggle Sidebar"
@@ -164,6 +166,7 @@ const AppHeader: React.FC = () => {
               </svg>
             )}
           </button>
+          */}
 
           {/* Logo mobile remplacé par le titre */}
           <div className="lg:hidden">
